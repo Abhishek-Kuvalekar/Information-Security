72fe500d0857985c5d63af1ba993992210c7beb8
Abhishek Kuvalekar <abhi.kuvalekar@gmail.com>
2018-09-07 22:24:10 +0530

    My own encryption algorithm

---
+++ Assignment-I/encryption.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+import sys
+
+def openFile(filename, mode):
+    try:
+        f = open(filename, mode)
+    except IOError as e:
+        print(e)
+    else:
+        return f
+
+def decryptFile(filename):
+    fd_r = openFile(filename, "r")
+    fd_w = openFile(filename.split(".")[0] + ".dec", "w")
+    if (fd_r == None) or (fd_w == None):
+        return
+    prev = None
+    curr = None
+    content = fd_r.read()
+    for character in content.split(", "):
+        character = character.strip()
+        if character == '':
+            continue
+        curr = int(character)
+        if prev == None:
+            prev = int(character)
+        else:
+            curr = (prev - curr)
+            prev = curr
+        fd_w.write(chr(curr))
+    fd_r.close()
+    fd_w.close()
+
+def encryptFile(filename):
+    fd_r = openFile(filename, "r")
+    fd_w = openFile(filename.split(".")[0] + ".enc", "w")
+    if (fd_r == None) or (fd_w == None):
+        return
+    prev = None
+    curr = None
+    for line in fd_r:
+        for character in line:
+            curr = ord(character)
+            if prev == None:
+                prev = ord(character)
+            else:
+                curr = (prev - curr)
+                prev = ord(character)
+            fd_w.write(str(curr) + ", ")
+    fd_r.close()
+    fd_w.close()
+
+def showUsage():
+    print("Usage: ./encryption.py -d|-e <filename.enc|filename.txt>")
+
+def main():
+    if(len(sys.argv) != 3):
+        showUsage()
+        return
+    if(sys.argv[1].lower() == "-e"):
+        encryptFile(sys.argv[2])
+    elif(sys.argv[1].lower() == "-d"):
+        decryptFile(sys.argv[2])
+    else:
+        showUsage()
+        return
+
+
+if __name__ == '__main__':
+    main()
